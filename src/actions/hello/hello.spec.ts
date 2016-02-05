2ef4d0eb54901f9a6b6a94e12107cec9c2dd445c
K.K. POON <noopkk@gmail.com>
2016-02-05 17:02:06 +0800

    add test for typescript example

---
+++ src/actions/hello/hello.spec.ts
@@ -0,0 +1,20 @@
+/// <reference path="../../../typings/main/ambient/mocha/mocha.d.ts" />
+/// <reference path="../../../typings/main/ambient/chai/chai.d.ts" />
+
+import { expect } from "chai";
+
+import updateHello from "./update";
+import clearHello from "./clear";
+
+describe("hello actions", () => {
+  it("should create an action to update hello message", () => {
+    const msg = "hello1";
+    const expected = {type: "UPDATE_HELLO", message: msg};
+    expect(updateHello(msg)).to.deep.equal(expected);
+  });
+
+  it("should create an action to clear hello message", () => {
+    const expected = {type: "CLEAR_HELLO"};
+    expect(clearHello()).to.deep.equal(expected);
+  });
+});
