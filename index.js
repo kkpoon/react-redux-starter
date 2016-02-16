65bab27859d7426ba7329f56ae55c954e4be81fc
K.K. POON <noopkk@gmail.com>
2016-02-16 19:13:18 +0800

    add Facebook Relay

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var falcorExpress = require('falcor-express');
+var graphqlHTTP = require('express-graphql');
+
 var Routers = require("./falcor-routers/routers");
+var helloSchemas = require("./graphql-schemas/hello");
 
 var app = express();
 
@@ -8,6 +11,8 @@ app.use('/model.json', falcorExpress.dataSourceRoute(function (req, res) {
   // create a Virtual JSON resource with single key ("greeting")
   return new Routers();
 }));
+
+app.use('/graphql', graphqlHTTP({ schema: helloSchemas, graphiql: true }));
 
 app.use(express.static("public"));
 
