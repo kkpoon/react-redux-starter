0566f846fd55d17ae29f03207cd2d495bdbb1c54
K.K. POON <noopkk@gmail.com>
2016-02-18 18:57:22 +0800

    webpack to build server app

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var webpack = require('webpack');
+var nodeExternals = require('webpack-node-externals');
 
 var plugins = [
   new webpack.DefinePlugin({
@@ -9,43 +10,72 @@ var plugins = [
   })
 ];
 
-module.exports = {
-  entry: "./src/web/index.jsx",
-  resolve: {
-    extensions: ["", ".ts", ".tsx", ".coffee", ".js", ".jsx", ".less"]
+module.exports = [
+  {
+    name: "web",
+    entry: "./src/web/index.jsx",
+    resolve: {
+      extensions: ["", ".ts", ".tsx", ".coffee", ".js", ".jsx", ".less"]
+    },
+    output: {
+      path: path.resolve(__dirname, 'public/lib'),
+      filename: "bundle.js"
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          loader: "babel-loader"
+        },
+        {
+          test: /\.tsx?$/,
+          loader: 'ts-loader'
+        },
+        {
+          test: /\.coffee$/,
+          loader: "coffee-loader"
+        },
+        {
+          test: /\.less$/,
+          loader: "style!css!less"
+        },
+        {
+          test: /\.css$/,
+          loader: "style!css"
+        },
+        {
+          test: /\.(png|woff|woff2|eot|ttf|svg)(\?.*)?$/,
+          loader: 'url-loader?limit=100000'
+        }
+      ]
+    },
+    plugins: plugins
   },
-  output: {
-    path: path.resolve(__dirname, 'public/lib'),
-    filename: "bundle.js"
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loader: "babel-loader"
-      },
-      {
-        test: /\.tsx?$/,
-        loader: 'ts-loader'
-      },
-      {
-        test: /\.coffee$/,
-        loader: "coffee-loader"
-      },
-      {
-        test: /\.less$/,
-        loader: "style!css!less"
-      },
-      {
-        test: /\.css$/,
-        loader: "style!css"
-      },
-      {
-        test: /\.(png|woff|woff2|eot|ttf|svg)(\?.*)?$/,
-        loader: 'url-loader?limit=100000'
-      }
-    ]
-  },
-  plugins: plugins
-};
+  {
+    name: "bin",
+    target: "node",
+    externals: [nodeExternals()],
+    entry: {
+      app: "./src/servers/app.js"
+    },
+    resolve: {
+      extensions: ["", ".js"]
+    },
+    output: {
+      path: path.resolve(__dirname, 'lib/servers'),
+      filename: "[name]",
+      libraryTarget: "commonjs2",
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: "babel-loader"
+        }
+      ]
+    },
+    plugins: plugins
+  }
+];
