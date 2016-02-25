59f4bf65158298f233bd46132a66c9b850a45232
K.K. POON <noopkk@gmail.com>
2016-02-25 15:46:27 +0800

    add Helmet to help changing the site title

--- src/web/pages/home.jsx
+++ src/web/pages/home.jsx
@@ -3,6 +3,7 @@ import colors from "material-colors";
 
 import React, { Component } from 'react';
 import { Grid, Cell } from "react-mdl";
+import Helmet from "react-helmet";
 
 import GreetingContainer from '../containers/greeting';
 import SayContainer from '../containers/say';
@@ -17,6 +18,7 @@ class HomePage extends Component {
   render() {
     return (
       <div>
+        <Helmet title="Home" />
         <Grid>
           <Cell col={7}>
             <GreetingContainer />
