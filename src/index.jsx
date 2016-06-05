0ad90f1aac65ad457225a6ee9e7b66f0aa3984e3
K.K. POON <noopkk@gmail.com>
2016-06-05 12:51:42 +0800

    refactor root.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { addLocaleData } from "react-intl";
+import { compose, createStore, combineReducers, applyMiddleware } from "redux";
+import { hashHistory, browserHistory } from "react-router";
+import { syncHistoryWithStore, routerReducer } from "react-router-redux";
 
-import Root from "./root";
+import routes from "./routes";
+import * as reducers from "./reducers";
+
+import Root from "./containers/root";
 
 import en from "react-intl/locale-data/en";
 import zh from "react-intl/locale-data/zh";
@@ -19,8 +25,33 @@ if (!global.Intl)
 else bootstrap();
 
 function bootstrap() {
+  const storeEnhancers = [];
+  const extensions = [];
+
+  if (__DEVTOOLS__) {
+    const DevTools = require("./components/dev-tools").default;
+    storeEnhancers.push(DevTools.instrument());
+    extensions.push(<DevTools key="devtools" />);
+  }
+
+  const createHistory = process.env.NODE_ENV === "production" ?
+    hashHistory : browserHistory;
+
+  const store = createStore(
+    combineReducers({
+      ...reducers,
+      routing: routerReducer,
+    }),
+    compose(
+      //applyMiddleware(),
+      ...storeEnhancers
+    )
+  );
+
+  const history = syncHistoryWithStore(createHistory, store);
+  
   ReactDOM.render(
-    <Root />,
+    <Root store={store} history={history} routes={routes} extensions={extensions} />,
     document.getElementById("react-root")
   );
 }
